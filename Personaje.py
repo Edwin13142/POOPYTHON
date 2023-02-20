bf0c98c65c6a48a172b616947c1a2c49d2e08f90
Edwin13142 <121038777@upq.edu.mx>
2023-02-20 16:59:52 -0600

    Se agregaron los metodos del personaje como correr, lanzar granadas y recargar arma

--- Personaje.py
+++ Personaje.py
@@ -4,3 +4,16 @@ class Personaje:
     especie = "Humano"
     nombre = "Medio Metro"
     altura = "1.40"
+
+    #metodos del personaje
+    def correr(self, status):
+        if(status):
+            print("El personaje"+self.nombre+ "esta corriendo")
+        else:
+            print("El personaje"+self.nombre+ "se detuvo")
+    def lanzargranadas(self):
+        print("El personaje"+self.nombre+ "lanzo una granada")
+    def recargararma(self, municiones):
+        cargador = 10
+        cargador = cargador+municiones
+        print("El arma tiene"+cargador+ "balas")
