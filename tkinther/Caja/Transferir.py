7714784057cdb8461722ebbf598eae95b0692144
Edwin13142 <121038777@upq.edu.mx>
2023-03-10 17:18:56 -0600

    Avance

---
+++ tkinther/Caja/Transferir.py
@@ -0,0 +1,8 @@
+from tkinter import Tk, Button, Frame, messagebox, Label, Entry
+class Transferir:
+    def __init__(self, numero_cuenta, titular, edad, saldo):
+        self.numero_cuenta = numero_cuenta
+        self.titular = titular
+        self.edad = edad
+        self.saldo = saldo
+    
