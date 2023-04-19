6d70967fb7c2888ae616949488518caf839fd69e
Edwin13142 <115176421+Edwin13142@users.noreply.github.com>
2023-04-19 17:13:36 -0600

    Programacion de examen solo falto consulta

--- Examen3erP/CRUD.py
+++ Examen3erP/CRUD.py
@@ -6,7 +6,17 @@ from Controlador import *
 control = controlador ()
 
 def registrar():
-    controlador.Registrar(varMer.get(), varPai.get())
+    control.Registrar(varMer.get(), varPais.get())
+def eliminar():
+    control.Eliminar(varID.get())
+def consulta():
+    rsUsuario = control.Consulta(str(varBus.get()))
+    for usu in rsUsuario:
+        cadena = str(usu[0])+" "+ str(usu[1])+ " "+ str(usu[2])
+    if(rsUsuario):
+        textBus.insert("0.0",cadena)
+    else:
+        messagebox.showerror("Error","Usuario no encontrado")
 
 Ventana = Tk()
 Ventana.title("Importaciones")
@@ -27,9 +37,9 @@ varMer = tk.StringVar()
 lblMer = Label(pestana1, text="Mercancia: ").pack(pady=5)
 txtMer = Entry(pestana1,textvariable=varMer).pack(pady=5)
 
-varPai = tk.StringVar()
+varPais = tk.StringVar()
 lblPais = Label(pestana1, text="Pais: ").pack(pady=5)
-txtPais = Entry(pestana1,textvariable=varPai).pack(pady=5)
+txtPais = Entry(pestana1,textvariable=varPais).pack(pady=5)
 
 btnRegistrar = Button(pestana1,text="Registrar", bg="Orange", command=registrar).pack(pady=5)
 
@@ -41,7 +51,7 @@ varID = tk.StringVar()
 lblID = Label(pestana2, text="ID: ").pack(pady=5)
 txtID = Entry(pestana2,textvariable=varID).pack(pady=5)
 
-btnEliminar = Button(pestana2,text="Eliminar", bg="Red", fg="White").pack(pady=5)
+btnEliminar = Button(pestana2,text="Eliminar", bg="Red", fg="White", command=eliminar).pack(pady=5)
 
 #Programacion de consulta
 
@@ -50,12 +60,11 @@ texto2 = Label(pestana3,text="Consulta por pais", fg="Black", font=("Modern",18)
 varBus = tk.StringVar()
 lblPas= Label(pestana3, text="Ingresa el pais: ").pack()
 txtPas = Entry(pestana3,textvariable=varBus).pack()
-btnBusqueda = Button(pestana3,text="Buscar").pack()
+btnBusqueda = Button(pestana3,text="Buscar", command=consulta).pack(pady=5)
 
 subBus = Label(pestana3,text="Registrado:",fg="Black",font=("Modern",18)).pack()
 textBus = tk.Text(pestana3,height=5,width=52)
 textBus.pack()
-
 
 panel.add(pestana1, text="Insertar:")
 panel.add(pestana2, text="Eliminar:")
--- Examen3erP/Controlador.py
+++ Examen3erP/Controlador.py
@@ -16,17 +16,64 @@ class controlador:
         except sqlite3.OperationalError:
             print("No se pudo conectar")
             
-    def Registrar(self, Mercancia, Pai):
+    def Registrar(self, Mercancia, Pais):
         
         conx = self.conexionBD()
         
-        if(Mercancia == "" or Pai == ""):
+        if(Mercancia == "" or Pais == ""):
             messagebox.showwarning("Error", "Campos vacios")
             conx.close()
         else:
             cursor = conx.cursor()
+            datos = (Mercancia, Pais)
             qrInsert="insert into TB_Europa(Mercancia,Pais) values(?,?)"
-            cursor.execute(qrInsert)
+            cursor.execute(qrInsert, datos)
             conx.commit()
             conx.close()
             messagebox.showinfo("Exito", "Se registro con exito")
+            
+    def Eliminar(self, ID):
+        
+        conx = self.conexionBD()
+        
+        if(ID ==""):
+            messagebox.showwarning("Error", "Campos vacios")
+            conx.close()
+        else:
+            try:
+                
+                cursor = conx.cursor()
+                sqlDelete = ("DELETE FROM TBR_Europa WHERE IDImpo = ?")
+                datos = (ID)
+                pregunta = messagebox.askokcancel("Confirmacion", "¿Deseas eliminar ese usuario?")
+                if pregunta == True:
+                    cursor.execute(sqlDelete,datos)
+                    conx.commit()
+                    conx.close()
+                    messagebox.showinfo("Exito", "Se elimino el usuario")
+                else:
+                    conx.close()
+                    messagebox.showerror("Error", "El Usuario no fue eliminado")
+                    
+            except sqlite3.OperationalError:
+                print("Error en la consulta")
+                
+    def Consulta(self, Pais):
+        
+        conx = self.conexionBD()
+        
+        if(Pais ==""):
+            messagebox.showwarning("Error", "Campos vacios")
+            conx.close()
+        else:
+            try:
+                cursor=conx.cursor()
+                sqlSelect = "select * from TBR_Europa where Pais="+Pais
+                cursor.execute(sqlSelect)
+                RSusuario = cursor.fetchall()
+                conx.close()
+                return RSusuario
+                
+            except sqlite3.OperationalError:
+                print("Error en la consulta")
+        
